ddd76b299e5d81497af3f356ca257dc6418be035
Ganesh007 <pas.apps.007@gmail.com>
2015-10-04 16:17:12 +0530

    Home Menu : Partial

--- EventApp/app/src/main/java/com/punehackers/eventapp/view/fragment/HomeFragment.java
+++ EventApp/app/src/main/java/com/punehackers/eventapp/view/fragment/HomeFragment.java
@@ -9,12 +9,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.punehackers.eventapp.R;
+import com.punehackers.eventapp.Widgets.CircleMenuLayout;
 import com.punehackers.eventapp.view.EventActivity;
 
 
 public class HomeFragment extends Fragment {
 
     private View rootView;
+
+    private CircleMenuLayout mCircleMenuLayout;
+
+    private String[] mItemTexts = new String[] { "BUS ", "CAB", "RICKSHAW",
+            "TRAIN"};
+    private int[] mItemImgs = new int[] { R.drawable.ic_bus,
+            R.drawable.ic_rickshaw, R.drawable.ic_rickshaw,
+            R.drawable.ic_train };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +39,50 @@ public class HomeFragment extends Fragment {
     private void initializeScreen()
     {
         ((EventActivity) getActivity()).mToolbar.setTitle("Home");
+
+        mCircleMenuLayout = (CircleMenuLayout) rootView.findViewById(R.id.id_menulayout);
+        mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);
+        mCircleMenuLayout.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+            @Override
+            public void itemClick(View view, int pos) {
+
+                switch (pos) {
+
+                    case 0:
+                        getActivity().getSupportFragmentManager().beginTransaction()
+                                .addToBackStack(null).add(R.id.container, new BusFragment()).commitAllowingStateLoss();
+                        break;
+                    case 1:
+                        getActivity().getSupportFragmentManager().beginTransaction()
+                                .addToBackStack(null).add(R.id.container, new BusFragment()).commitAllowingStateLoss();
+                        break;
+
+                    case 2:
+                        getActivity().getSupportFragmentManager().beginTransaction()
+                                .addToBackStack(null).add(R.id.container, new RickshawFragment()).commitAllowingStateLoss();
+                        break;
+
+                    case 3:
+                        getActivity().getSupportFragmentManager().beginTransaction()
+                                .addToBackStack(null).add(R.id.container, new TrainFragment()).commitAllowingStateLoss();
+                        break;
+
+                    default:
+                        getActivity().getSupportFragmentManager().beginTransaction()
+                                .addToBackStack(null).add(R.id.container, new BusFragment()).commitAllowingStateLoss();
+                        break;
+
+                }
+
+
+            }
+
+            @Override
+            public void itemCenterClick(View view) {
+
+            }
+        });
+
     }
 
 }
